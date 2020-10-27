31692e19582b8eea47f0a12439387d12f5d6465a
siemen <siemensubbaiah1@gmail.com>
2020-10-27 10:06:09 +0530

    added a comment

--- src/youtube.js
+++ src/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = process.env.REACT_APP_KEY;
+const KEY = process.env.REACT_APP_KEY; //YOUR YT DATA API KEY
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
