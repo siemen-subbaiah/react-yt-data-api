89c119d34760e239468298ed97bb7e8e1ca0f716
siemen <siemensubbaiah1@gmail.com>
2021-12-04 21:33:38 +0530

    added notice

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import VideoList from './VideoList';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 const Videos = ({ videos, loading }) => {
   return (
     <>
+      <h3 className='text-center'>
+        Use the search bar above to search videos and click on the videos to get
+        the dislike count
+      </h3>
+      <p className='text-center'>
+        Please use this wisely 🙏 as there is less quota from Youtube API{' '}
+        <YouTubeIcon
+          style={{
+            color: 'red',
+            position: 'relative',
+            top: '0.3rem',
+          }}
+        />
+      </p>
       {loading ? (
         <h3 className='load'>FETCHING RESULTS FROM YOUTUBE API...</h3>
       ) : (
